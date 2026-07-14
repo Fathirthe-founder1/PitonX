44cbed87247cb416c2c2711d3ac5f80148ae3a7e
Jameson Greogory Void <fathiragata22@gmail.com>
2026-07-15 00:59:56 +0700

    Rename package from 'piton' to 'PitonX'

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='piton',
+    name='PitonX',
     version='1.0.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'piton = piton.cli:main'
+            'pitonx = pitonx.cli:main'
         ]
     },
     description='Bahasa Pemrograman Indonesia berbasis Python',
     author='Fathirthe-founder1',
-    url='https://github.com/Fathirthe-founder1/piton',
+    url='https://github.com/Fathirthe-founder1/PitonX',
     python_requires='>=3.6',
 )
